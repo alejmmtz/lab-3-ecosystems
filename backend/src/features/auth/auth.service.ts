94f57bb00f4ca810c2a093643813d225a12e1126
Alejandro <alejomum@gmail.com>
2026-03-18 01:17:52 -0500

    fix: auth

--- backend/src/features/auth/auth.service.ts
+++ backend/src/features/auth/auth.service.ts
@@ -42,7 +42,7 @@ export const createUserService = async (
     const { error: storeError } = await supabase.from('stores').insert([
       {
         name: user.storeName,
-        owner_Id: newUserId,
+        owner_id: newUserId,
         status: 'closed',
       },
     ]);
